0afb3a5b47ee05134d2f396ddbaf78b5011aaec3
Tina Humphrey <humphrey14t@gmail.com>
2024-04-04 19:03:43 -0400

    commit changes to front end specifications files: SignupForm.jsx, App.jsx, and main.jsx to errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
-// Create an Apollo Provider to make every request work with the Apollo Server.
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// TODO: Create an Apollo Provider to make every request work with the Apollo Server.
 import './App.css';
+// Important for API Consumption: To enable interaction with our GraphQL API on the front end, we utilize these tools to develop the client-side behavior
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
+import { Outlet } from 'react-router-dom';
+import React from 'react';
 import Navbar from './components/Navbar';
+
+// import SearchBooks from './pages/SearchBooks';
+// import SavedBooks from './pages/SavedBooks';
+
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -15,16 +18,19 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <Navbar />  
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </Router>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Navbar />
+        <div className="container">
+        
+            <Outlet />  
+        </div>    
+          {/* <SearchBooks />
+          <SavedBooks /> */}
+      </div>
     </ApolloProvider>
   );
 }
 
+
 export default App;
+
